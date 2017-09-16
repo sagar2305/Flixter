4928a893af6dd0a0510bcce4edbcc4dc5ee8e4a2
Sagar Mutha <sagar2305@gmail.com>
2017-09-16 10:54:26 -0700

    implement title and overview in custom view

--- app/src/main/java/com/flixter/flixter/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/flixter/flixter/adapters/MovieArrayAdapter.java
@@ -1,8 +1,16 @@
 package com.flixter.flixter.adapters;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.flixter.flixter.R;
 import com.flixter.flixter.models.Movie;
 
 import java.util.ArrayList;
@@ -15,5 +23,33 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
 
     public MovieArrayAdapter(Context context, ArrayList<Movie> movies) {
         super(context, android.R.layout.simple_list_item_1 , movies);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        // get movie
+        Movie movie = getItem(position);
+
+        // check to see if existing view is being reused
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+        }
+
+        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+        //clear out image from imageview
+        ivImage.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+
+        //populate data
+        tvTitle.setText(movie.getOriginalTitle());
+        tvOverview.setText(movie.getOverview());
+
+        //return view
+        return convertView;
     }
 }
